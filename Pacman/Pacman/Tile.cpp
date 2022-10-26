2979b8b1807adb08d71a0a83f40aa3f307a3fc98
StBiccy <115167027+StBiccy@users.noreply.github.com>
2022-10-26 20:15:28 +0100

    this was a mission that I failed

--- Pacman/Pacman/Tile.cpp
+++ Pacman/Pacman/Tile.cpp
@@ -5,9 +5,10 @@ const int Tile::_cWidth = 32;
 
 const Vector2* Tile::_cSize = new Vector2((float)_cWidth, (float)_cHight);
 
-Tile::Tile(Texture2D* _Texture)
+Tile::Tile(Texture2D* _Texture, tileCollision _Collision)
 {
 	_texture = _Texture;
+	_collision = _Collision;
 }
 
 Tile::~Tile(void)
