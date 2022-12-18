a4a48cdc33409ec786db328652c56871423fa5ad
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2022-12-18 15:38:32 +0000

    Revert "finished Bish"
    This reverts commit c58211dcee03fad5dec6d75b36a2064971e383f8.

--- JumpKing/Pacman/Player.h
+++ JumpKing/Pacman/Player.h
@@ -12,7 +12,6 @@
 
 // Just need to include main header file
 #include "S2D/S2D.h"
-#include <fstream>;
 
 // Reduces the amount of typing by including all classes in S2D namespace
 using namespace S2D;
@@ -26,9 +25,6 @@ private:
 	Level* _level;
 
 	int _currentScene;
-
-	ifstream recallSave;
-	ofstream sendSave;
 
 	// Data to represent animation
 	bool _walking;
